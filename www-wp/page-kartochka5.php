cd05c1533f1ccc9b6d711760b8ce75b9f28faa62
Glomberg <Bazz@bk.ru>
2015-05-26 16:46:41 +0300

    95%

---
+++ www-wp/page-kartochka5.php
@@ -0,0 +1,17 @@
+<?php
+/*
+*Template Name: Для "Карточка-СЕР.МЕТАЛЛИК"
+*/
+get_header(); ?>
+<?php action_of_week(); ?>
+<div class="articles">
+	<?php side_bar(); ?>
+	<?php iinclude_page(62); ?>
+	<a class="oformit-knopka" href="http://dushalekseeva1.ru/?page_id=33">Оформить заказ</a>
+		<script>
+			$(document).ready(function(){
+				
+			});
+		</script>	
+</div>
+<?php get_footer(); ?>
